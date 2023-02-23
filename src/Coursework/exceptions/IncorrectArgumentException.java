efefceee01021cc485a727d8e9c9542607e13e99
lexxxxa82 <lexxxxa.82@mail.ru>
2023-02-23 13:17:55 +0800

    3

---
+++ src/Coursework/exceptions/IncorrectArgumentException.java
@@ -0,0 +1,19 @@
+package Coursework.exceptions;
+
+public class IncorrectArgumentException extends Exception {
+
+    public IncorrectArgumentException(String argument) {
+        this.argument = argument;
+    }
+
+    private final String argument;
+
+    public String getArgument() {
+        return argument;
+    }
+
+    @Override
+    public String toString() {
+        return argument;
+    }
+}
